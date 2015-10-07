905675b7e9b2d07c747d684ef5cdc84a9ae92ab9
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-07 09:41:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-filebased-email-backend-ng',
     version=1,
-    packages=(
-        'django_filebased_email_backend_ng',
-    )
+    description="A better 'file' email backend for Django",
+
+    url="https://chris-lamb.co.uk/projects/django-filebased-email-backend-ng",
+    author="Chris Lamb",
+    author_email='chris@chris-lamb.co.uk',
+    license="BSD",
+
+    packages=find_packages(),
 )
