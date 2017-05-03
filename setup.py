111924b3cf7bb0c4c5e6e20f3d59eb55e6b835de
Chris Lamb <chris@chris-lamb.co.uk>
2017-05-03 20:09:19 +0100

    Releasing version 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-filebased-email-backend-ng',
-    version='2.0.2',
+    version='2.1.0',
     description="A better 'file' email backend for Django",
 
     url="https://chris-lamb.co.uk/projects/django-filebased-email-backend-ng",
